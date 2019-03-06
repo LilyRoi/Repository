6069d0438254a47429d1416f645fdf5532756e18
Maria <m.a.r@mail.ru>
2019-03-06 23:52:19 +0300

    Added Lesson 11-12 homework

---
+++ Lesson 11-12/Field.java
@@ -0,0 +1,18 @@
+package Lesson11home_work;
+
+public class Field {
+	Integer id;
+	String value;
+	
+	public Field(Integer id, String value) {
+		this.id = id;
+		this.value = value;
+	}
+	
+	public String toString() {
+		return "Field: [id=" + id + "; value=" + value + "]";
+	}
+
+
+
+}
---
+++ Lesson 11-12/Label.java
@@ -0,0 +1,16 @@
+package Lesson11home_work;
+
+public class Label {
+	String name;
+	
+	Label (String name){
+		this.name = name;		
+	}
+	
+	public String toString() {
+		return "Label: [name=" + name + "]";
+	}
+
+
+
+}
